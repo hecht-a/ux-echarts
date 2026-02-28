9c1a2ce6f888f7c84a876a4f5b16017b48f6e0a2
HECHT Axel <axel.hct@gmail.com>
2026-02-28 19:41:08 +0100

    ref: qa tools & tests

---
+++ .php-cs-fixer.php
@@ -0,0 +1,31 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+    ->append([__DIR__ . '/castor.php'])
+    ->exclude(['cache', 'Resources/stubs'])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->registerCustomFixers((new PhpCsFixerCustomFixers\Fixers()))
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'concat_space' => ['spacing' => 'one'],
+        'yoda_style' => false,
+        'native_constant_invocation' => false,
+        'no_superfluous_phpdoc_tags' => [
+            'remove_inheritdoc' => false,
+        ],
+        'declare_strict_types' => true,
+        'no_trailing_comma_in_singleline' => false,
+        'function_declaration' => ['trailing_comma_single_line' => true],
+        'phpdoc_to_comment' => ['allow_before_return_statement' => true],
+        'psr_autoloading'  => false,
+        'multiline_promoted_properties' => true,
+    ])
+    ->setFinder($finder)
+    ;
---
+++ bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+
+\Symfony\Bridge\PhpUnit\DeprecationErrorHandler::register("max[self]=0&max[direct]=0&verbose=1");
